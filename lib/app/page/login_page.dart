05848df06a99f3daa8350c6ced4766dbbbcaca1c
Guilherme Daldim <gdaldims@gmail.com>
2022-07-11 14:59:53 -0300

    Add Button Go Home + Update version

--- lib/app/page/login_page.dart
+++ lib/app/page/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:micro_commons_design_system/micro_commons_design_system.dart';
+import 'package:micro_core/app/micro_core_utils.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,6 +10,12 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
+      ),
+      body: Center(
+        child: CustomButtonWidget(
+          text: 'Go To Home',
+          onPressed: () => navigatorKey.currentState!.pushNamed('/home'),
+        ),
       ),
     );
   }
